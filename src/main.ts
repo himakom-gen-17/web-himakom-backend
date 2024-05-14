cc779930bbbf01f6e3336d188da649c1c81e45f6
Akbar Dzikri <akbardzkr05@gmail.com>
2024-05-15 02:16:10 +0700

    added swagger for api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Web Himakom API')
+    .setDescription('Dokumentasi API Website Himakom Stikom Poltek Cirebon')
+    .setVersion('1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
