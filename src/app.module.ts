282db3806c190ccb3774e44eb46c2aa605d4fee7
Akbar Dzikri <akbardzkr05@gmail.com>
2024-06-01 03:24:24 +0700

    added routes for fetching categories

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import { GenerasiModule } from './generasi/generasi.module';
 import { DivisiModule } from './divisi/divisi.module';
 import { ArticlesModule } from './articles/articles.module';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { ArticlesModule } from './articles/articles.module';
     GenerasiModule,
     DivisiModule,
     ArticlesModule,
+    CategoriesModule,
   ],
 })
 export class AppModule {}
